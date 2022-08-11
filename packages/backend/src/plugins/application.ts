8c6a9cb70b8d5bf1b19f51c7d5b70cdb4a028565
keisuke <kihara@vertigo.com.br>
2022-08-11 11:51:00 -0300

    feature get services and stand alone server generate

--- packages/backend/src/plugins/application.ts
+++ packages/backend/src/plugins/application.ts
@@ -5,12 +5,9 @@ import { PluginEnvironment } from '../types';
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  // Here is where you will add all of the required initialization code that
-  // your backend plugin needs to be able to start!
-
-  // The env contains a lot of goodies, but our router currently only
-  // needs a logger
   return await createRouter({
+    config:env.config,
     logger: env.logger,
+    database:env.database
   });
 }
