a9a87c52dbb554e696df56e27d588242de9b8de5
luccas <luccasmtxr@gmail.com>
2022-11-18 16:42:54 -0300

    new docker-compose

--- plugins/application-backend/knexfile.js
+++ plugins/application-backend/knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: { user: 'youruser', database: 'backstage_plugin_application', password: 'yourpassword'}
+    connection: { user: 'user', database: 'devportal', password: 'password'}
   },
   production: { 
     client: 'pg', 
