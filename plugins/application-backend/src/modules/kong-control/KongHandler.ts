9806121107fa98eed1910606c21ef18a26a18477
JFelixLima <jlima@vertigo.com.br>
2022-11-23 13:04:38 -0300

    listConsumer | Migrate partner

--- plugins/application-backend/src/modules/kong-control/KongHandler.ts
+++ plugins/application-backend/src/modules/kong-control/KongHandler.ts
@@ -34,11 +34,10 @@ export class KongHandler {
     return response ? servicesStore.map((service:Service)=> service.name) : [];
   }
 
-  public async listConsumers(tls:false,kongUrl:string): Promise<Service[]> {
-    const url = tls ? `https://${kongUrl}/services` : `http://${kongUrl}/services`;
+  public async listConsumers(kongUrl:string, tls:false) {
+    const url = tls ? `https://${kongUrl}/consumers` : `http://${kongUrl}/consumers`;
     const response = await axios.get(url);
-    const servicesStore = response.data.data;
-    return response ? servicesStore.map((service:Service)=> service.name) : [];
+    return response.data.data;
   }
 
   public async generateCredential(tls:false, kongUrl: string, workspace: string, idConsumer: string){
