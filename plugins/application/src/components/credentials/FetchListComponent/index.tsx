050b780ce95262aa15f56796ac25ec76ffdbaebe
ValberJunior <valberjr1992@gmail.com>
2022-12-09 19:04:45 -0300

    application review

--- plugins/application/src/components/credentials/FetchListComponent/index.tsx
+++ plugins/application/src/components/credentials/FetchListComponent/index.tsx
@@ -6,59 +6,102 @@ import { Button } from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import useAsync from 'react-use/lib/useAsync';
 import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline';
-// Mock
-import Mock from '../Mock/mock.json';
 import AlertComponent from '../../Alert/Alert';
+import { ICredentials } from '../interfaces';
 
-// type DenseTableProps = {
-//   applications: IApplication[];
-// };
-
-export const DenseTable = ({ handleClick }: any) => {
-
-  const columns: TableColumn[] = [
-    { title: 'Id', field: 'id', width: '1fr' },
-    { title: 'Key', field: 'key', width: '1fr' },
-    { title: 'Actions', field: "actions", width: '1fr' }
-  ];
-
-  const data = Mock.map(item => {
-    return {
-      id: item.id,
-      key: item.key,
-      actions: <Button variant='outlined' onClick={handleClick} component={RouterLink} to="" style={{ border: 'none' }}> <DeleteOutlineIcon /> </Button>
-    };
-  });
-
-  return (
-    <Table
-      title="All Credentials"
-      options={{ search: true, paging: true }}
-      columns={columns}
-      data={data}
-      style={{ width: "100%", border: "none" }}
-    />
-  );
+type DenseTableProps = {
+  credentials: ICredentials[];
 };
 
-export const FetchListComponent = () => {
-
+export const DenseTable = ({ credentials }: DenseTableProps) => {
   const [show, setShow] = useState<boolean>(false);
+  const [status, setStatus] = useState<string>('');
+  const [messageStatus, setMessageStatus] = useState<string>('');
 
   const handleClose = (reason: string) => {
     if (reason === 'clickaway') return;
     setShow(false);
   };
 
-  const generateCredential = () => {
-    setShow(true)
-    // eslint-disable-next-line no-console
-    console.log(show)
-  }
-  const { value, loading, error } = useAsync(async (): Promise<any[]> => {
-    const response = await fetch('http://localhost:7007/api/application/');
+  const columns: TableColumn[] = [
+    { title: 'Id', field: 'id', width: '1fr' },
+    { title: 'Key', field: 'key', width: '1fr' },
+    { title: 'Actions', field: 'actions', width: '1fr' },
+  ];
+
+  // remove Credential    ======================================================================================> //TO DO
+  const removeCredential = async (ID: string) => {
+    const config = {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    };
+    const response = await fetch(
+      `http://localhost:7007/api/application/credential/${ID}`,
+      config,
+    );
+    if (response.status === 200) {
+      setShow(true);
+      setStatus('success');
+      setMessageStatus('Credential created!');
+    }
+    // eslint-disable-next-line no-alert
+    if (response.status !== 200) {
+      setShow(true);
+      setStatus('error');
+      setMessageStatus('An error has occurred');
+    }
+  };
+
+  const data = credentials.map(item => {
+    return {
+      id: item.id,
+      key: item.key,
+      actions: (
+        <Button
+          variant="outlined"
+          onClick={() => removeCredential(item.id)}
+          component={RouterLink}
+          to=""
+          style={{ border: 'none' }}
+        >
+          {' '}
+          <DeleteOutlineIcon />{' '}
+        </Button>
+      ),
+    };
+  });
+
+  return (
+    <>
+      <AlertComponent
+        open={show}
+        close={handleClose}
+        message={messageStatus}
+        status={status}
+      />
+      <Table
+        title="All Credentials"
+        options={{ search: true, paging: true }}
+        columns={columns}
+        data={data}
+        style={{ width: '100%', border: 'none' }}
+      />
+    </>
+  );
+};
+
+export const FetchListComponent = ({ idConsumer }: { idConsumer: string }) => {
+  // list Credentias
+  const { value, loading, error } = useAsync(async (): Promise<
+    ICredentials[]
+  > => {
+    const response = await fetch(
+      `http://localhost:7007/api/application/credencial/${idConsumer}`,
+    );
     const data = await response.json();
-    return data.applications;
+    return data.credentials;
   }, []);
 
   if (loading) {
@@ -67,10 +110,5 @@ export const FetchListComponent = () => {
     return <Alert severity="error">{error.message}</Alert>;
   }
 
-  return (
-    <>
-      <AlertComponent open={show} close={handleClose} message="Credential deleted!" />
-      <DenseTable handleClick={generateCredential} />
-    </>
-  );
+  return <DenseTable credentials={value || []} />;
 };
--- plugins/application/src/components/devApplication/ApplicationDetailsComponent/DetailsComponent/index.tsx
+++ plugins/application/src/components/devApplication/ApplicationDetailsComponent/DetailsComponent/index.tsx
@@ -1,6 +1,11 @@
+/* eslint-disable no-console */
 import React, { PropsWithChildren, useState } from 'react';
 import { Grid, Button } from '@material-ui/core';
-import { CardTab, StructuredMetadataTable, TabbedCard } from '@backstage/core-components';
+import {
+  CardTab,
+  StructuredMetadataTable,
+  TabbedCard,
+} from '@backstage/core-components';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Link as RouterLink } from 'react-router-dom';
 import { Credentials } from '../../../credentials';
@@ -8,18 +13,28 @@ import AlertComponent from '../../../Alert/Alert';
 
 const cardContentStyle = { heightX: 'auto', width: '100%', marginLeft: '2%' };
 
-
 const Wrapper = ({ children }: PropsWithChildren<{}>) => (
-  <Grid container spacing={4} style={{ width: '100%', marginTop: '.5em', display: 'flex', justifyContent: 'center' }}>
-    <Grid item lg={12}>{children}</Grid>
+  <Grid
+    container
+    spacing={4}
+    style={{
+      width: '100%',
+      marginTop: '.5em',
+      display: 'flex',
+      justifyContent: 'center',
+    }}
+  >
+    <Grid item lg={12}>
+      {children}
+    </Grid>
   </Grid>
 );
 
 type Props = {
   metadata: any[] | any;
   back: string;
-  remove?: string
-}
+  remove?: string;
+};
 
 export default {
   title: 'Layout/Tabbed Card',
@@ -33,22 +48,42 @@ export default {
   ],
 };
 
-
 export const DetailsComponent = ({ metadata, back, remove }: Props) => {
-
   const [show, setShow] = useState<boolean>(false);
+  const [status, setStatus] = useState<string>('');
+  const [messageStatus, setMessageStatus] = useState<string>('');
+
+  const kongConsumerId = metadata ? metadata.kongConsumerId : '';
 
   const handleClose = (reason: string) => {
     if (reason === 'clickaway') return;
     setShow(false);
   };
 
-  const generateCredential = () => {
-    setShow(true)
-    // eslint-disable-next-line no-console
-    console.log(show)
-  }
-
+  // generate Credentials
+  const generateCredential = async (ID: string) => {
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    };
+    const response = await fetch(
+      `http://localhost:7007/api/application/credential/${ID}`,
+      config,
+    );
+    if (response.status === 200) {
+      setShow(true);
+      setStatus('success');
+      setMessageStatus('Credential created!');
+    }
+    // eslint-disable-next-line no-alert
+    if (response.status !== 200) {
+      setShow(true);
+      setStatus('error');
+      setMessageStatus('An error has occurred');
+    }
+  };
 
   return (
     <Wrapper>
@@ -57,22 +92,76 @@ export const DetailsComponent = ({ metadata, back, remove }: Props) => {
           <TabbedCard>
             <CardTab label="About">
               <StructuredMetadataTable metadata={metadata} dense={false} />
-              <Grid container justifyContent='center' alignItems='center' spacing={2} style={{ padding: '3em 0' }}>
-                <Grid item><Button component={RouterLink} to={back} variant='contained' size='large'>Cancel</Button></Grid>
-                {remove && <Grid item><Button component={RouterLink} to={remove} variant='contained' size='large'>Remove</Button></Grid>}
+              <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                style={{ padding: '3em 0' }}
+              >
+                <Grid item>
+                  <Button
+                    component={RouterLink}
+                    to={back}
+                    variant="contained"
+                    size="large"
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
+                {remove && (
+                  <Grid item>
+                    <Button
+                      component={RouterLink}
+                      to={remove}
+                      variant="contained"
+                      size="large"
+                    >
+                      Remove
+                    </Button>
+                  </Grid>
+                )}
               </Grid>
             </CardTab>
             <CardTab label="Credentials">
-              <AlertComponent open={show} close={handleClose} message="Credential created!" />
-              <Credentials />
-              <Grid container justifyContent='center' alignItems='center' spacing={2} style={{ padding: '3em 0' }}>
-                <Grid item><Button onClick={generateCredential} variant='contained' size='large'>New Credential Key</Button></Grid>
-                <Grid item><Button component={RouterLink} to={back} variant='contained' size='large'>Cancel</Button></Grid>
+              <AlertComponent
+                open={show}
+                close={handleClose}
+                message={messageStatus}
+                status={status}
+              />
+              <Credentials idConsumer={kongConsumerId} />
+              <Grid
+                container
+                justifyContent="center"
+                alignItems="center"
+                spacing={2}
+                style={{ padding: '3em 0' }}
+              >
+                <Grid item>
+                  <Button
+                    onClick={() => generateCredential(kongConsumerId)}
+                    variant="contained"
+                    size="large"
+                  >
+                    New Credential Key
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button
+                    component={RouterLink}
+                    to={back}
+                    variant="contained"
+                    size="large"
+                  >
+                    Cancel
+                  </Button>
+                </Grid>
               </Grid>
             </CardTab>
           </TabbedCard>
         </Wrapper>
       </div>
     </Wrapper>
-  )
+  );
 };
