4df6727af8322a6fccae72699cd234898a7532df
julopesrocha <jujulrocha@gmail.com>
2022-12-12 16:08:23 -0300

    merging to consumer service branch

--- plugins/application-backend/src/modules/kong/services/ConsumerGroupService.ts
+++ plugins/application-backend/src/modules/kong/services/ConsumerGroupService.ts
@@ -1,53 +1,52 @@
 import axios from 'axios';
-import { kongHeaders } from '../../utils/KongHeaders';
 import { ConsumerGroup } from '../model/ConsumerGroup';
 import { KongServiceBase } from './KongServiceBase';
 
 export class ConsumerGroupService extends KongServiceBase {
 
     public async createConsumerGroup(consumerGroup: ConsumerGroup): Promise<ConsumerGroup> {
-        const url = `${this.url}/${this.workspace}/consumer_groups`;
+        const url = `${this.baseUrl}/consumer_groups`;
         const response = await axios
             .post(url, consumerGroup, {
-                headers: kongHeaders(this.adminToken),
+                headers: this.getAuthHeader(),
             })
         return response.data.consumerGroup;
     }
 
     public async listConsumerGroups(): Promise<ConsumerGroup[]> {
-        const url = `${this.url}/consumer_groups`;
+        const url = `${this.baseUrl}/consumer_groups`;
         const response = await axios
             .get(url, {
-                headers: kongHeaders(this.adminToken),
+                headers: this.getAuthHeader(),
             })
         const groups = response.data.data;
         return groups.map((group:ConsumerGroup) => group);
     }
     
     public async deleteConsumerGroup(consumerGroupId: string): Promise<ConsumerGroup> {
-        const url = `${this.url}/consumer_groups/${consumerGroupId}`;
+        const url = `${this.baseUrl}/consumer_groups/${consumerGroupId}`;
         const response = await axios
           .delete(url, {
-            headers: kongHeaders(this.adminToken),
+            headers: this.getAuthHeader(),
           })
         const consumerGroup = response.data;
         return consumerGroup;
     }
 
     public async addConsumerToGroup(consumerGroupId:string, consumerId:string) {
-        const url = `${this.url}/consumer_groups/${consumerGroupId}/consumers`;
+        const url = `${this.baseUrl}/consumer_groups/${consumerGroupId}/consumers`;
         const response = await axios
             .post(url, consumerId, {
-                headers: kongHeaders(this.adminToken),
+                headers: this.getAuthHeader(),
             })
         return response.data;
     }
 
     public async removeConsumerFromGroups(consumerId:string){
-        const url = `${this.url}/consumers/${consumerId}/consumer_groups`;
+        const url = `${this.baseUrl}/consumers/${consumerId}/consumer_groups`;
         const response = await axios
           .delete(url, {
-            headers: kongHeaders(this.adminToken),
+            headers: this.getAuthHeader(),
           })
         const consumerGroup = response.data;
         return consumerGroup;
@@ -56,10 +55,10 @@ export class ConsumerGroupService extends KongServiceBase {
     public async removeConsumerFromGroup(consumerId:string, consumerGroupId:string){
         // Remove a consumer from a consumer group
 
-        const url = `${this.url}/consumers/${consumerId}/consumer_groups/${consumerGroupId}`;
+        const url = `${this.baseUrl}/consumers/${consumerId}/consumer_groups/${consumerGroupId}`;
         const response = await axios
           .delete(url, {
-            headers: kongHeaders(this.adminToken),
+            headers: this.getAuthHeader(),
           })
         const consumerGroup = response.data;
         return consumerGroup;
