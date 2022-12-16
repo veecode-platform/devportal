9542113513772774bbee763404297e25768173f5
andreluas <andreluas.k@gmail.com>
2022-12-16 11:01:16 -0300

    chore: implements rateLimiting field in service database, update migrations and seeds files

--- plugins/application-backend/seeds/23112022_seed.js
+++ plugins/application-backend/seeds/23112022_seed.js
@@ -14,6 +14,7 @@ exports.seed = async function(knex) {
                 partnersId: ["ad94b906-4970-11ed-b878-0242ac120001", "27447ec1-d67f-4233-bfb1-0f11f7ccca28"],
                 kongServiceName: "kong service test",
                 kongServiceId: "916723c1-b79c-4b32-929e-2ea1327f0525",
+                rateLimiting: "a2e8d096-2bf8-4d7a-9c3a-ee55eae3379e",
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
@@ -25,6 +26,7 @@ exports.seed = async function(knex) {
                 partnersId: ["ad94b906-4970-11ed-b878-0242ac120001", "27447ec1-d67f-4233-bfb1-0f11f7ccca28"],
                 kongServiceName: "kong service test 2",
                 kongServiceId: "89dbedab-5b38-460c-a0eb-92898f45090b",
+                rateLimiting: "a2e8d096-2bf8-4d7a-9c3a-ee55eae3379e",
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
@@ -36,6 +38,7 @@ exports.seed = async function(knex) {
                 partnersId: ["ad94b906-4970-11ed-b878-0242ac120001", "27447ec1-d67f-4233-bfb1-0f11f7ccca28"],
                 kongServiceName: "kong service test 3",
                 kongServiceId: "491f34e7-15bc-4d68-add1-e0b8a71762b0",
+                rateLimiting: "a2e8d096-2bf8-4d7a-9c3a-ee55eae3379e",
                 createdAt: new Date(),
                 updatedAt: new Date()
             },
