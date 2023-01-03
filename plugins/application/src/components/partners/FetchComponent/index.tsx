5e95cbec8fd62ff42b2b3d46bec7ac341a3dbb2c
ValberJunior <valberjr1992@gmail.com>
2023-01-03 12:53:53 -0300

    partner plugin refactoring -- application plugin

--- plugins/application/src/components/partners/FetchComponent/index.tsx
+++ plugins/application/src/components/partners/FetchComponent/index.tsx
@@ -1,30 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Table, TableColumn, Progress} from '@backstage/core-components';
 import { Link as RouterLink} from 'react-router-dom';
 import { Button} from '@material-ui/core';
 import Alert from '@material-ui/lab/Alert';
 import useAsync from 'react-use/lib/useAsync';
-
-
-type Partner = {
-  id: string; 
-  name: string;
-  applicationId: Array<string>; 
-  createdAt: string; 
-  updatedAt: string; 
-};
+import { IPartner } from '../interfaces';
+import More from '@material-ui/icons/MoreHorizOutlined';
 
 type DenseTableProps = {
-  partners: Partner[];
+  partners: IPartner[];
 };
 
 export const DenseTable = ({ partners }: DenseTableProps) => {
 
   const columns: TableColumn[] = [
-    { title: 'Name', field: 'name' },
-    { title: 'Id', field: 'id' },
-    { title: "Created At", field: "created"},
-    { title: 'Details', field: 'details' },
+    { title: 'Name', field: 'name', width:'1fr' },
+    { title: 'Id', field: 'id', width:'1fr' },
+    { title: "Created At", field: "created", width:'1fr'},
+    { title: 'Details', field: 'details', width: '1fr' },
   ];
 
   const data = partners.map(partner => {
@@ -32,14 +26,14 @@ export const DenseTable = ({ partners }: DenseTableProps) => {
       name: partner.name,
       id: partner.id,
       created: partner.createdAt,
-      details: <Button variant='outlined' component={RouterLink} to={`/partners/partner-details?id=${partner.id}`}>&gt;&gt;&gt;</Button>
+      details: <Button variant='outlined' component={RouterLink} to={`/partners/partner-details?id=${partner.id}`}><More/></Button>
     };
   });
 
   return (
     <Table
-      title="All partners"
-      options={{ search: false, paging: true }}
+      title={`All partners (${partners.length})`}
+      options={{ search: true, paging: true }}
       columns={columns}
       data={data}
     />
@@ -47,10 +41,9 @@ export const DenseTable = ({ partners }: DenseTableProps) => {
 };
 
 export const FetchComponent = () => {
-  const { value, loading, error } = useAsync(async (): Promise<Partner[]> => {
+  const { value, loading, error } = useAsync(async (): Promise<IPartner[]> => {
     const response = await fetch('http://localhost:7007/api/application/partners');
     const data = await response.json();
-    //console.log(data)
     return data.partners;
   }, []);
 
