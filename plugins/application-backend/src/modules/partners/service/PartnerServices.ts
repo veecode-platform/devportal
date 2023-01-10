aa623dc32f32c2d72e79fc3657c7748f1930ec18
andreluas <andreluas.k@gmail.com>
2023-01-10 11:48:41 -0300

    chore: active partner, change active applications and services method

--- plugins/application-backend/src/modules/partners/service/PartnerServices.ts
+++ plugins/application-backend/src/modules/partners/service/PartnerServices.ts
@@ -1,7 +1,11 @@
 import { RouterOptions } from '../../../service/router';
+import { ApplicationDto } from '../../applications/dtos/ApplicationDto';
+import { PostgresApplicationRepository } from '../../applications/repositories/knex/KnexApplicationRepository';
 import { ApplicationServices } from '../../applications/services/ApplicationServices';
 import { UserDto } from '../../keycloak/dtos/UserDto';
 import { KeycloakUserService } from '../../keycloak/service/UserService';
+import { ServiceDto } from '../../services/dtos/ServiceDto';
+import { PostgresServiceRepository } from '../../services/repositories/Knex/KnexServiceReppository';
 import { ControllPlugin } from '../../services/service/ControllPlugin';
 import { PartnerDto } from '../dtos/PartnerDto';
 import { PostgresPartnerRepository } from '../repositories/Knex/KnexPartnerReppository';
@@ -67,6 +71,12 @@ export class PartnerServices {
       const partnerRepository = await PostgresPartnerRepository.create(
         await options.database.getClient(),
       );
+      const applicationRepository = await PostgresApplicationRepository.create(
+        await options.database.getClient(),
+      );
+      const serviceRepository = await PostgresServiceRepository.create(
+        await options.database.getClient(),
+      );
       const partner = await partnerRepository.getPartnerById(partnerId);
 
       if (partner instanceof Object) {
@@ -82,6 +92,46 @@ export class PartnerServices {
             keycloakUser.id,
             userDto,
           );
+        }
+
+        if (partnerDto.active === false) {
+          const applications = partnerDto.applicationId as string[];
+          applications.forEach(async application => {
+            const applicationObj =
+              await applicationRepository.getApplicationById(application);
+            if (applicationObj instanceof Object) {
+              let applicationDto = new ApplicationDto(
+                applicationObj.name as string,
+                applicationObj.creator as string,
+                applicationObj.parternId as string,
+                applicationObj.servicesId as string[],
+                (applicationObj.active = false),
+              );
+              await applicationRepository.patchApplication(
+                application,
+                applicationDto,
+              );
+            }
+          });
+        }
+
+        if (partnerDto.active === false) {
+          const services = partnerDto.servicesId as string[];
+          services.forEach(async service => {
+            const serviceObj = await serviceRepository.getServiceById(service);
+            if (serviceObj instanceof Object) {
+              let serviceDto = new ServiceDto(
+                serviceObj.name as string,
+                serviceObj.description as string,
+                serviceObj.partnersId as string[],
+                serviceObj.redirectUrl as string,
+                serviceObj.kongServiceName as string,
+                serviceObj.kongServiceId as string,
+                (serviceObj.active = false),
+              );
+              await serviceRepository.patchService(service, serviceDto);
+            }
+          });
         }
       }
     } catch (error) {
