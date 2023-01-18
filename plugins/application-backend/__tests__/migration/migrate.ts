a811fe5ee9068ed0a3308ab1e89e799a04dc4ca6
andreluas <andreluas.k@gmail.com>
2023-01-18 14:28:08 -0300

    chore: implements migrations for tests

---
+++ plugins/application-backend/__tests__/migration/migrate.ts
@@ -0,0 +1,21 @@
+import knexFactory from 'knex';
+import { StartedTestContainer } from 'testcontainers';
+
+export const db = (container: StartedTestContainer) => {
+  return knexFactory({
+    client: 'pg',
+    connection: {
+      user: 'test',
+      password: 'test',
+      database: 'devportal_tests',
+      host: container.getHost(),
+      port: container.getMappedPort(5432),
+    },
+  });
+};
+
+export async function applyMigrations(container: StartedTestContainer) {
+  await db(container).migrate.latest({
+    directory: __dirname + './../../migrations',
+  });
+}
