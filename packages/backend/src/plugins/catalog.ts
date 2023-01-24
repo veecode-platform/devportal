c2553fa4768654c25bd2df4c009ce28cfa030082
luccas <luccasmtxr@gmail.com>
2023-01-24 11:35:53 -0300

    add gitlab catalog discovery and integration & remove unused fields in app-config.yaml

--- packages/backend/src/plugins/catalog.ts
+++ packages/backend/src/plugins/catalog.ts
@@ -1,4 +1,5 @@
 import { GithubEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
+//import { GitlabDiscoveryEntityProvider } from '@backstage/plugin-catalog-backend-module-gitlab';
 import { CatalogBuilder} from '@backstage/plugin-catalog-backend';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
@@ -14,10 +15,21 @@ builder.addEntityProvider(
     schedule: env.scheduler.createScheduledTaskRunner({
       frequency: { minutes: 30 },
       timeout: { minutes: 3 },
-    }),
-    
+    }), 
   }),
-);
+)
+// gitlab provider
+//builder.addEntityProvider(
+//  GitlabDiscoveryEntityProvider.fromConfig(env.config, {
+//    logger: env.logger,
+//    // optional: alternatively, use scheduler with schedule defined in app-config.yaml
+//    schedule: env.scheduler.createScheduledTaskRunner({
+//      frequency: { minutes: 2 },
+//      timeout: { minutes: 3 },
+//    }),
+//    //scheduler: env.scheduler,
+//  }),
+//);
 
 const { processingEngine, router } = await builder.build();
 await processingEngine.start();
