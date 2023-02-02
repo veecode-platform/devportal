5e8d2160ad488a40c547bda5a8b7ea98fc70e6da
JFelixLima <jlima@vertigo.com.br>
2023-02-02 13:53:53 -0300

    create keycloack-router.ts

--- plugins/application-backend/src/service/router.ts
+++ plugins/application-backend/src/service/router.ts
@@ -21,6 +21,7 @@ import { createApplicationRouter } from './applications-route';
 import { applyDatabaseMigrations } from '../../database/migrations';
 import { testeRoute } from './teste-router';
 import { createPluginRouter } from './plugins-route';
+import { createKeycloackRouter } from './keycloak-router';
 
 /** @public */
 export interface RouterOptions {
@@ -42,8 +43,6 @@ export async function createRouter(
 
   await applyDatabaseMigrations(await database.getClient());
 
-  const adminClientKeycloak = new TestGroups();
-  const userServiceKeycloak = new KeycloakUserService();
   const consumerGroupService = new ConsumerGroupService();
 
 
@@ -56,74 +55,8 @@ export async function createRouter(
   router.use('/kong-extras', await createKongRouter(options))
   router.use('/applications', await createApplicationRouter(options))
   router.use('/plugins', await createPluginRouter(options))
+  router.use('/keycloack', await createKeycloackRouter())
   router.use('/teste', await testeRoute(options))
-
-  // KEYCLOAK
-  router.get('/keycloak/groups', async (_, response) => {
-    const groups = await adminClientKeycloak.getGroup();
-    response.status(200).json({ status: 'ok', groups: groups });
-  });
-
-  router.post('/keycloak/users', async (request, response) => {
-    const user: UserDto = request.body.user;
-    const id = await userServiceKeycloak.createUser(user);
-    response.status(201).json({ status: 'ok', id: id });
-  });
-
-  router.get('/keycloak/users', async (_, response) => {
-    const users = await userServiceKeycloak.listUsers();
-    response.status(200).json({ status: 'ok', users: users });
-  });
-
-  router.get('/keycloak/users/:id', async (request, response) => {
-    const user_id = request.params.id;
-    const user = await userServiceKeycloak.findUser(user_id);
-    response.status(200).json({ status: 'ok', users: user });
-  });
-
-  router.put('/keycloak/users/:id', async (request, response) => {
-    const code = request.params.id;
-    const user: UpdateUserDto = request.body.user;
-    await userServiceKeycloak.updateUser(code, user as UserDto);
-    response.status(200).json({ status: 'User Updated!' });
-  });
-
-  router.delete('/keycloak/users/:id', async (request, response) => {
-    const user_id = request.params.id;
-    await userServiceKeycloak.deleteUser(user_id);
-    response.status(204).json({ status: 'User Deleted!' });
-  });
-
-  router.put(
-    '/keycloak/users/:id/groups/:groupId',
-    async (request, response) => {
-      const user_id = request.params.id;
-      const groupId = request.params.groupId;
-      const add = await userServiceKeycloak.addUserToGroup(user_id, groupId);
-      response.status(200).json({ status: 'User added to group!', add: add });
-    },
-  );
-
-  router.delete(
-    '/keycloak/users/:id/groups/:groupId',
-    async (request, response) => {
-      const user_id = request.params.id;
-      const groupId = request.params.groupId;
-      const res = await userServiceKeycloak.removeUserFromGroup(
-        user_id,
-        groupId,
-      );
-      response
-        .status(204)
-        .json({ status: 'User Removed From Group!', res: res });
-    },
-  );
-
-  router.get('/keycloak/users/:id/groups', async (request, response) => {
-    const user_id = request.params.id;
-    const groups = await userServiceKeycloak.listUserGroups(user_id);
-    response.status(200).json({ status: 'ok', groups: groups });
-  });
 
   // CONSUMER GROUP
 
