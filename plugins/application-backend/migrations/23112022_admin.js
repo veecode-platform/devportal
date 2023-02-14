3f310faea95ab4d796b3718d8cea52d1fef81619
JFelixLima <jlima@vertigo.com.br>
2023-02-14 13:43:36 -0300

    add foreign key

--- plugins/application-backend/migrations/23112022_admin.js
+++ plugins/application-backend/migrations/23112022_admin.js
@@ -4,47 +4,48 @@
 
 exports.up = async function up(knex) {
   try {
+
     await knex.schema.createTable('services', table => {
       table.uuid('id').primary();
       table.string('name');
       table.boolean('active');
       table.string('description');
       table.string('redirectUrl');
-      table.specificType('partnersId', 'TEXT[]');
       table.integer('rateLimiting');
       table.string('kongServiceName');
       table.string('kongServiceId');
       table
-        .enu('securityType', ['none', 'key-auth', 'oauth2'], {
-          useNative: true,
-          enumName: 'security_type',
+      .enu('securityType', ['none', 'key-auth', 'oauth2'], {
+        useNative: true,
+        enumName: 'security_type',
         })
         .defaultTo('none');
-      table.timestamp('createdAt').defaultTo(knex.fn.now());
-      table.timestamp('updatedAt').defaultTo(knex.fn.now());
-    });
-    await knex.schema.createTable('partners', table => {
-      table.uuid('id').primary();
-      table.string('name');
-      table.boolean('active');
-      table.string('email');
-      table.string('phone');
-      table.specificType('servicesId', 'TEXT[]');
-      table.specificType('applicationId', 'TEXT[]'); // lista de applications criadas
-      table.timestamp('createdAt').defaultTo(knex.fn.now());
-      table.timestamp('updatedAt').defaultTo(knex.fn.now());
-    });
-    await knex.schema.createTable('applications', table => {
-      table.uuid('id').primary();
-      table.string('name');
-      table.boolean('active');
-      table.string('creator');
-      table.specificType('parternId', 'TEXT');
-      table.specificType('servicesId', 'TEXT[]'); // lista de services que a application usa
-      table.string('externalId');
-      table.timestamp('createdAt').defaultTo(knex.fn.now());
-      table.timestamp('updatedAt').defaultTo(knex.fn.now());
-    });
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+      });
+      
+ 
+      await knex.schema.createTable('partners', table => {
+        table.uuid('id').primary();
+        table.string('name');
+        table.boolean('active');
+        table.string('email');
+        table.string('phone');
+        // lista de applications criadas
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+      });
+
+ 
+      await knex.schema.createTable('applications', table => {
+        table.uuid('id').primary();
+        table.string('name');
+        table.boolean('active');
+        table.string('creator');// lista de services que a application usa
+        table.string('externalId');
+        table.timestamp('createdAt').defaultTo(knex.fn.now());
+        table.timestamp('updatedAt').defaultTo(knex.fn.now());
+      });
     await knex.schema.createTable('plugins', table => {
       table.uuid('id').primary();
       table.string('name');
@@ -57,6 +58,20 @@ exports.up = async function up(knex) {
       table.timestamp('updatedAt').defaultTo(knex.fn.now());
       table.foreign('service').references('services.id').onDelete('CASCADE');
     });
+    await knex.schema.alterTable('applications', table => {
+      table.uuid('servicesId').unsigned().references('id').inTable('services');
+      table.uuid('partnersId').unsigned().references('id').inTable('partners'); 
+    })
+    await knex.schema.alterTable('partners', table => {
+      table.uuid('servicesId').unsigned().references('id').inTable('services')  
+      table.uuid('applicationId').unsigned().references('id').inTable('applications') 
+    })
+    await knex.schema.alterTable('services', table => {
+      table.uuid('partnersId').unsigned().references('id').inTable('partners')  
+    })
+    await knex.schema.alterTable('plugins', table => {
+      table.uuid('partnersId').unsigned().references('id').inTable('partners')  
+    })
   } catch (e) {
     console.log('ERROR MIGRATE:UP ', e);
     return false;
