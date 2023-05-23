8bafc0a087e311f1568b04551593b44f69424899
ValberJunior <valberjr1992@gmail.com>
2023-05-23 18:32:53 -0300

    update service

--- plugins/scaffolder/src/components/Services/providers/github/index.ts
+++ plugins/scaffolder/src/components/Services/providers/github/index.ts
@@ -6,25 +6,28 @@ export async function getOrgsGithub(token:string): Promise<string[] | undefined>
     const headers = {
         Authorization: `Bearer ${token}`
     }
+    const orgsList = [];
+
     try{
         const response = await axios.get( GITHU_URL ,{ headers });
 
         if (response.status === 200){
             const orgs = response.data;
-            const orgsList = [];
             for( const org of orgs){
-                orgsList.push(org.login as string)
+                orgsList.push(org.login as string);
             }
             return orgsList
         }
         else {
             // lançar um erro  --- ver se vou retornar um array vazio ou disparar um erro
             console.log('Error');
-            return ["Not orgs"]
+            orgsList.push("Not orgs");
+            return orgsList;
         }
     }
     catch(error){
-        console.log('Error')
-        return ["Not orgs"]
+        console.log('Error');
+        orgsList.push("Not orgs");
+        return orgsList;
     }
 }
--- plugins/scaffolder/src/components/Services/providers/gitlab/index.ts
+++ plugins/scaffolder/src/components/Services/providers/gitlab/index.ts
@@ -6,25 +6,28 @@ export async function getOrgsGitlab(token:string): Promise<string[] | undefined>
     const headers = {
         'Private-Token': token
     }
+    const orgsList = [];
+
     try{
         const response = await axios.get( GITLAB_URL ,{ headers });
 
         if (response.status === 200){
             const orgs = response.data;
-            const orgsList = [];
             for( const org of orgs){
-                orgsList.push(org.name as string)
+                orgsList.push(org.name as string);
             }
-            return orgsList
+            return orgsList;
         }
         else {
             // lançar um erro  --- ver se vou retornar um array vazio ou disparar um erro
             console.log('Error');
-            return ["Not Orgs"]
+            orgsList.push("Not Orgs");
+            return orgsList;
         }
     }
     catch(error){
-        console.log('Error')
-        return ["Not Orgs"]
+        console.log('Error');
+        orgsList.push("Not Orgs");
+        return orgsList;
     }
 }
