6a5f8ec5a4e2a25f277e707da650e464be428b89
ValberJunior <valberjr1992@gmail.com>
2023-06-21 15:15:45 -0300

    update sign up function

--- packages/app/src/components/Root/signUp/index.tsx
+++ packages/app/src/components/Root/signUp/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { SidebarItem } from '@backstage/core-components';
 import SignOutIcon from '@material-ui/icons/MeetingRoom';
-import { SessionState, configApiRef, githubAuthApiRef, gitlabAuthApiRef, identityApiRef, oktaAuthApiRef, useApi } from '@backstage/core-plugin-api';
-import { keycloakOIDCAuthApiRef } from '../../../apis';
+import { configApiRef, identityApiRef, useApi } from '@backstage/core-plugin-api';
 
 
 const SignUpElement = () => {
@@ -11,43 +10,18 @@ const SignUpElement = () => {
   const config = useApi(configApiRef);
   const providersConfig = config.getOptionalConfig('auth.providers');
   const configuredProviders = providersConfig?.keys() || [];
-  const gitlab = useApi(gitlabAuthApiRef);
-  const github = useApi(githubAuthApiRef);
-  const okta = useApi(oktaAuthApiRef);
-  const keycloak = useApi(keycloakOIDCAuthApiRef);
 
   const handleSessionLogout = async () => { 
-        if (configuredProviders.includes('keycloak')){
-            const status = keycloak.sessionState$();
-            status.subscribe((sessionState) => {
-            const sessionStateString = sessionState === SessionState.SignedIn ? "SignedIn" : "SignedOut";
-            if (sessionStateString === SessionState.SignedIn) keycloak.signOut() 
-        });
-        }
-
-        if (configuredProviders.includes('okta')){
-            const status = okta.sessionState$();
-            status.subscribe((sessionState) => {
-            const sessionStateString = sessionState === SessionState.SignedIn ? "SignedIn" : "SignedOut";
-            if (sessionStateString === SessionState.SignedIn) okta.signOut() 
-            });
-        }
-
-        if (configuredProviders.includes('github')){
-            const status = github.sessionState$();
-            status.subscribe((sessionState) => {
-            const sessionStateString = sessionState === SessionState.SignedIn ? "SignedIn" : "SignedOut";
-            if (sessionStateString === SessionState.SignedIn) github.signOut() 
-            });
-        }
-        if (configuredProviders.includes('gitlab')){
-            const status = gitlab.sessionState$();
-            status.subscribe((sessionState) => {
-            const sessionStateString = sessionState === SessionState.SignedIn ? "SignedIn" : "SignedOut";
-            if (sessionStateString === SessionState.SignedIn) gitlab.signOut() 
-            });
-        }
-    }
+    const token = await identityApi.getCredentials();
+    const backendBaseUrl = config.getConfig('backend').get('baseUrl');
+    if (configuredProviders.includes('keycloak')){
+     await fetch(`${backendBaseUrl}/api/devportal/keycloak/logout`, {
+      method: "GET",
+      headers:{ Authorization: `Bearer ${token.token}`}
+    })
+     }
+     // add new providers as they are used
+}
 
   return (
     <SidebarItem
@@ -55,7 +29,7 @@ const SignUpElement = () => {
      text="Sign Out" 
      onClick={async () => {
             if (!config.getBoolean('platform.guest.enabled')) {await handleSessionLogout()}
-            identityApi.signOut()
+            identityApi.signOut();
             }}/>
   )
 }
