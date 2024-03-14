444272a883cc3903fd579d5791a1d9f598cb8724
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-03-14 08:42:45 -0300

    update gitlab proxy config

--- chart/generate-docs.sh
+++ chart/generate-docs.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 script_path=$(dirname "$0")
+
+#check if frigate is installed and install it if not
+if ! command -v frigate &> /dev/null
+then
+    echo "frigate could not be found, installing it now"
+    if command -v pip &> /dev/null
+    then
+        pip install frigate
+    elif command -v pip3 &> /dev/null
+    then
+        pip3 install frigate
+    else
+        echo "pip or pip3 could not be found, please install it manually"
+        exit 1
+    fi
+fi
+
 cp ${script_path}/values.yaml ${script_path}/values-tmp.yaml
 
 # Read the file line by line
