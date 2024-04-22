f72e71c0b07809b9c7b6192b62389350c0ee8c83
ValberJunior <valberjr1992@gmail.com>
2024-04-22 09:20:27 -0300

    scaffolder config to config.d.ts

--- packages/app/config.d.ts
+++ packages/app/config.d.ts
@@ -393,10 +393,37 @@ export interface Config {
       readOnlyMode: boolean
     }
   }
-  // /**
-  //  * 
-  //  * @visibility frontend
-  //  */
-  // kong: {
-  // }
+  /**
+  * Configuration for scaffolder towards various external repository provider systems
+  * @visibility frontend
+  */
+  scaffolder?: {
+    /** Integration configuration for GitHub */
+    github?: Array<{
+      /**
+       * The hostname of the given GitHub instance
+       * @visibility frontend
+       */
+      host: string;
+      /**
+       * Token used to authenticate requests.
+       * @visibility frontend
+       */
+      token?: string;
+    }>;
+
+    /** Integration configuration for Gitlab */
+    gitlab?: Array<{
+      /**
+       * The hostname of the given Gitlab instance
+       * @visibility frontend
+       */
+      host: string;
+      /**
+       * Token used to authenticate requests.
+       * @visibility frontend
+       */
+      token?: string;
+    }>;
+  };
 }
