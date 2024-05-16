d55b7d09cd9f61133a8d6859337caee16f9fb40d
luccas <luccasmtxr@gmail.com>
2024-05-16 10:16:43 -0300

    add license key chart values & update api

--- packages/app/src/context/AppProvider.tsx
+++ packages/app/src/context/AppProvider.tsx
@@ -1,4 +1,4 @@
-import React,{ ReactNode, useState } from "react";
+import React,{ ReactNode, useEffect, useState } from "react";
 import { AppContext } from "./AppContext";
 import { licenseKeyApiRef } from "@internal/backstage-plugin-support"
 import { useApi } from '@backstage/core-plugin-api';
@@ -11,14 +11,20 @@ interface AppProviderProps {
 
 export const AppProvider:React.FC<AppProviderProps> = ({children})=> {
     const licenseKeyApi = useApi(licenseKeyApiRef);
+    const [showAlert, setShowAlert] = useState<boolean>(true); 
+
     const { value } = useAsync(async (): Promise<any> => {
         const license = await licenseKeyApi.validateLicenseKey()
         return license
       }, []);
-    const [showAlert, setShowAlert] = useState<boolean>(!value?.valid_key); 
 
-    const handleShowAlert = ()=>
+    useEffect(()=> {
+      setShowAlert(!value?.valid_key)
+    }, [value])
+
+    const handleShowAlert = () => {
       setShowAlert(false)
+    }
 
     return (
         <AppContext.Provider
