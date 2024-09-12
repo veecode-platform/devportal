d41c6fe37f89adc9be575c163ddc040bd7baf4a5
luccas <luccasmtxr@gmail.com>
2024-09-12 16:19:13 -0300

    update frontend dependencies

--- packages/backend/src/types.ts
+++ packages/backend/src/types.ts
@@ -5,7 +5,6 @@ import {
   PluginDatabaseManager,
   PluginEndpointDiscovery,
   TokenManager,
-  UrlReader,
 } from '@backstage/backend-common';
 import { PluginTaskScheduler } from '@backstage/backend-tasks';
 import { PermissionEvaluator } from '@backstage/plugin-permission-common';
@@ -16,7 +15,6 @@ export type PluginEnvironment = {
   database: PluginDatabaseManager;
   cache: PluginCacheManager;
   config: Config;
-  reader: UrlReader;
   discovery: PluginEndpointDiscovery;
   tokenManager: TokenManager;
   scheduler: PluginTaskScheduler;
