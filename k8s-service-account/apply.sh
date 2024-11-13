d4d6b4c9cc5878211c17b6273c3fcfe979bdb86b
Luan Ricardo Gazin <luan_gazin@yahoo.com.br>
2024-11-13 14:49:56 -0300

    update service-account k8s plugin

--- k8s-service-account/apply.sh
+++ k8s-service-account/apply.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-# check if namespace vkpr exists and create it if not
-kubectl get namespace vkpr || kubectl create namespace vkpr
+# check if namespace veecode-apr exists and create it if not
+export NAMESPACE=${1:-default}
+export CLUSTER_ROLE_NAME="platform-devportal-kubernetes-$NAMESPACE-read-only"
+export SERVICE_ACCOUNT_NAME="platform-devportal-kubernetes-read-only"
+kubectl get namespace $NAMESPACE || kubectl create namespace $NAMESPACE
+yq eval '''
+    .metadata.name = env(CLUSTER_ROLE_NAME) 
+''' -i cluster-role.yaml
+yq eval '''
+    .metadata.name = env(CLUSTER_ROLE_NAME) |
+    .roleRef.name = env(CLUSTER_ROLE_NAME) |
+    .subjects[].name = env(SERVICE_ACCOUNT_NAME) |
+    .subjects[].namespace = env(NAMESPACE)
+''' -i cluster-role-binding.yaml
+yq eval '''
+    .metadata.name = env(SERVICE_ACCOUNT_NAME) |
+    .metadata.namespace = env(NAMESPACE)
+''' -i service-account.yaml
 
 kubectl apply -f ./cluster-role.yaml
 kubectl apply -f ./service-account.yaml
 kubectl apply -f ./cluster-role-binding.yaml
 
-SERVICE_ACCOUNT_NAME=$(cat ./service-account.yaml | yq -e '.metadata.name')
-SERVICE_ACCOUNT_NAMESPACE=$(cat ./service-account.yaml | yq -e '.metadata.namespace // "default"')
-echo "Generating token for $SERVICE_ACCOUNT_NAME namespace $SERVICE_ACCOUNT_NAMESPACE"
-SERVICE_ACCOUNT_TOKEN=$(kubectl create token ${SERVICE_ACCOUNT_NAME} -n ${SERVICE_ACCOUNT_NAMESPACE})
+echo "Generating token for $SERVICE_ACCOUNT_NAME namespace ${NAMESPACE}"
+SERVICE_ACCOUNT_TOKEN=$(kubectl create token ${SERVICE_ACCOUNT_NAME} --duration=87660h -n ${NAMESPACE})
 
-## Getting current server authority certificate
+# ## Getting current server authority certificate
 K8S_SERVER_CERTIFICATE=$(cat ~/.kube/config | yq -r ".clusters[] | select(.name == \"$(cat ~/.kube/config | yq -r '.current-context')\").cluster.certificate-authority-data")
 K8S_SERVER_HOST=$(cat ~/.kube/config | yq -r ".clusters[] | select(.name == \"$(cat ~/.kube/config | yq -r '.current-context')\").cluster.server")
 echo "-----------------------------------"
--- k8s-service-account/remove.sh
+++ k8s-service-account/remove.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-kubectl delete -f ./cluster-role-binding.yaml
-kubectl delete -f ./service-account.yaml
-kubectl delete -f ./cluster-role.yaml
+export NAMESPACE=$1
+#if NAMESPACE is not set, ask for it
+if [ -z "$NAMESPACE" ]; then
+  read -p "Enter the namespace: " NAMESPACE
+fi
+echo "Deleting service account in namespace $NAMESPACE"
+
+
+kubectl delete -f ./cluster-role-binding.yaml -n $NAMESPACE
+kubectl delete -f ./service-account.yaml -n $NAMESPACE
+kubectl delete -f ./cluster-role.yaml -n $NAMESPACE
