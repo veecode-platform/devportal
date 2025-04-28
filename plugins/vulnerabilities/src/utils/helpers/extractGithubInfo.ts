4c8d4fb51dcfc10bdd257fe38c7421b5b0b2d785
ValberJunior <valberjr1992@gmail.com>
2025-04-28 01:23:10 -0300

    feat: ✨ implement extractGitHubInfo function for parsing GitHub URLs

---
+++ plugins/vulnerabilities/src/utils/helpers/extractGithubInfo.ts
@@ -0,0 +1,21 @@
+export function extractGitHubInfo (url:string){
+
+    const regex =
+      /https:\/\/([^/]+)\/([^/]+)\/([^/]+)\/(blob|tree)\/([^/]+)(?:\/(.*))?/;
+    const match = url.match(regex);
+  
+    if (!match) {
+      throw new Error('URL inválida');
+    }
+  
+    const [_, host, owner, repo, type, branch, path] = match;
+  
+    return {
+      host,
+      owner,
+      repo,
+      type,
+      branch,
+      path: path || '',
+    };
+  };
