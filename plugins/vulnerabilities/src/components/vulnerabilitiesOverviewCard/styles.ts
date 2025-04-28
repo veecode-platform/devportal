24852bb33e817da4e46cef1e8688ecc389849202
ValberJunior <valberjr1992@gmail.com>
2025-04-28 01:22:07 -0300

    feat: ✨ add styles for VulnerabilitiesOverviewCard component

---
+++ plugins/vulnerabilities/src/components/vulnerabilitiesOverviewCard/styles.ts
@@ -0,0 +1,32 @@
+import { makeStyles } from "@material-ui/core";
+
+export const useVulnerabilitiesStyles = makeStyles(theme=> ({
+    root:{
+        backgroundColor: theme.palette.background.paper,
+        padding: '2rem 1rem',
+        borderRadius: '10px',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        gap: '1.5rem',
+        flexDirection: 'column'
+    },
+    vulnerabilitiesChips:{
+        width: '95%',
+        margin: 'left',
+        display: 'flex',
+        alignItems: 'cemter',
+        justifyContent: 'center',
+        gap: '.5rem'
+    },
+    footer:{
+        alignSelf:"flex-end"
+    },
+    buttonStyle:{
+        background: "transparent",
+        outline: "none",
+        border: "none",
+        cursor: 'pointer',
+        color: theme.palette.text.primary
+    }
+}))
