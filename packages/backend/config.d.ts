fabe6c5a7037cd6bce75412e3e1bafdca3d2990c
ValberJunior <valberjr1992@gmail.com>
2025-06-24 17:05:50 -0300

    Refactor Config interface to improve structure and add detailed documentation for plugins and notifications

--- packages/backend/config.d.ts
+++ packages/backend/config.d.ts
@@ -16,138 +16,155 @@
 
 /** Configuration for the devportal plugin behavior */
 export interface Config {
-    /**
-    * 
-    * @visibility frontend
-    */
-    kong?: {
-        instances?: Array<{
-            id: string;
-            apiBaseUrl: string;
-            workspace: string;
-            auth: {
-                kongAdmin?: string,
-                custom?: {
-                    header: string,
-                    value: string
-                }
-            }
-        }>;
-    },
-    vee?:{
-        openai?: {
-          apiBaseUrl: string;
-          apiKey: string;
-          model: string;
-        }
-      }
-    /**
-     * 
-     * @visibility frontend
-     */
-    enabledPlugins: {
-        /**
-         * vault launch control.
-         * @visibility frontend
-         */
-        rbac: boolean
-        /**
-         * vault launch control.
-         * @visibility frontend
-         */
-        vault: boolean
-
-        /**
-         * vault kubernetes control.
-         * @visibility frontend
-         */
-        kubernetes: boolean
-
-        /**
-         * vault launch control.
-         * @visibility frontend
-         */
-        grafana: boolean
-        /**
-         * gitlabPlugin launch control.
-         * @visibility frontend
-         */
-        gitlabPlugin: boolean
-
-        /**
-         * keycloakPlugin launch control.
-         * @visibility frontend
-         */
-        keycloak: boolean
-        /**
-         * AzureDevops Plugin launch control.
-         * @visibility frontend
-         */
-        azureDevops: boolean
-        /**
-         * Kong Plugin launch control.
-         * @visibility frontend
-         */
-        kong: boolean
-        /**
-         * Vee Plugin launch control.
-         * @visibility frontend
-         */
-         vee: boolean
-        /**
-         * Sonarqube Plugin launch control.
-         * @visibility frontend
-         */
-        sonarqube: boolean
+  /**
+   *
+   * @visibility frontend
+   */
+  kong?: {
+    instances?: Array<{
+      id: string;
+      apiBaseUrl: string;
+      workspace: string;
+      auth: {
+        kongAdmin?: string;
+        custom?: {
+          header: string;
+          value: string;
+        };
+      };
+    }>;
+  };
+  vee?: {
+    openai?: {
+      apiBaseUrl: string;
+      apiKey: string;
+      model: string;
     };
+  };
+  /**
+   *
+   * @visibility frontend
+   */
+  enabledPlugins: {
     /**
-     * 
+     * vault launch control.
      * @visibility frontend
      */
-    platform: {
-        /**
-        * 
-        * @visibility frontend
-        */
-        signInProviders: Array<string>,  
-        guest: {
-            /**
-             * 
-             * @visibility frontend
-             */
-            enabled: boolean
-            /**
-             * 
-             * @visibility frontend
-             */
-            demo: boolean
-        };
-        defaultGroup: {
-            /**
-             * 
-             * @visibility frontend
-             */
-            enabled: boolean
-        };
-        groups: {
-            /**
-             * 
-             * @visibility frontend
-             */
-            admin: string
-            /**
-             * 
-             * @visibility frontend
-             */
-            user: string
-        };
-        apiManagement: {
-            /**
-             * 
-             * @visibility frontend
-             */
-            enabled: boolean
-        }
-    },
-}
+    rbac: boolean;
+    /**
+     * vault launch control.
+     * @visibility frontend
+     */
+    vault: boolean;
 
+    /**
+     * vault kubernetes control.
+     * @visibility frontend
+     */
+    kubernetes: boolean;
+
+    /**
+     * vault launch control.
+     * @visibility frontend
+     */
+    grafana: boolean;
+    /**
+     * gitlabPlugin launch control.
+     * @visibility frontend
+     */
+    gitlabPlugin: boolean;
+
+    /**
+     * keycloakPlugin launch control.
+     * @visibility frontend
+     */
+    keycloak: boolean;
+    /**
+     * AzureDevops Plugin launch control.
+     * @visibility frontend
+     */
+    azureDevops: boolean;
+    /**
+     * Kong Plugin launch control.
+     * @visibility frontend
+     */
+    kong: boolean;
+    /**
+     * Vee Plugin launch control.
+     * @visibility frontend
+     */
+    vee: boolean;
+    /**
+     * Sonarqube Plugin launch control.
+     * @visibility frontend
+     */
+    sonarqube: boolean;
+  };
+  /**
+   *
+   * @visibility frontend
+   */
+  platform: {
+    /**
+     *
+     * @visibility frontend
+     */
+    signInProviders: Array<string>;
+    guest: {
+      /**
+       *
+       * @visibility frontend
+       */
+      enabled: boolean;
+      /**
+       *
+       * @visibility frontend
+       */
+      demo: boolean;
+    };
+    defaultGroup: {
+      /**
+       *
+       * @visibility frontend
+       */
+      enabled: boolean;
+    };
+    groups: {
+      /**
+       *
+       * @visibility frontend
+       */
+      admin: string;
+      /**
+       *
+       * @visibility frontend
+       */
+      user: string;
+    };
+    apiManagement: {
+      /**
+       *
+       * @visibility frontend
+       */
+      enabled: boolean;
+    };
+  };
+  /**
+   * Configuration options for notifications-backend
+   */
+  notifications?: {
+    /**
+     * Concurrency limit for notification sending, defaults to 10
+     */
+    concurrencyLimit?: number;
+    /**
+     * Throttle duration between notification sending, defaults to 50ms
+     */
+    throttleInterval?: HumanDuration | string;
+    /**
+     * Time to keep the notifications in the database, defaults to 365 days.
+     * Can be disabled by setting to false.
+     */
+    retention?: HumanDuration | string | false;
+  };
+}
