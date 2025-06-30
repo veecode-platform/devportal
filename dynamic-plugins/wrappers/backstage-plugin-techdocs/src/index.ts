1006386b64dec7137b1e9b6f48d3c11b7acb8c2b
ValberJunior <valberjr1992@gmail.com>
2025-06-30 18:24:20 -0300

    Remove TechDocs plugin dependencies and related components from the app

---
+++ dynamic-plugins/wrappers/backstage-plugin-techdocs/src/index.ts
@@ -0,0 +1,7 @@
+export * from "@backstage/plugin-techdocs";
+export {
+  EntityTechdocsContent,
+  TechDocsReaderPage,
+  TechdocsSearchFilter,
+  techdocsSearchType,
+} from "./wrapped";
---
+++ dynamic-plugins/wrappers/roadiehq-backstage-plugin-github-pull-requests/src/index.ts
@@ -0,0 +1,2 @@
+export * from '@roadiehq/backstage-plugin-github-pull-requests';
+
