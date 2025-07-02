92b2afa8d40937cf2bcfbee447ddd9e4adb4eefb
luccas mantuano <luccasmtxr@gmail.com>
2025-07-02 13:06:30 -0300

    fix grid type

--- packages/app/src/components/catalog/Grid/Grid.tsx
+++ packages/app/src/components/catalog/Grid/Grid.tsx
@@ -17,6 +17,52 @@
  * limitations under the License.
  */
 
+import React from 'react';
+import Box, { BoxProps } from '@mui/material/Box';
+import { makeStyles } from 'tss-react/mui';
+
+const useStyles = makeStyles()(theme => ({
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3),
+    gridAutoFlow: 'dense',
+    alignItems: 'start',
+  },
+}));
+
+type GridProps = React.PropsWithChildren<
+  { container?: boolean; item?: boolean } & BoxProps
+>;
+
+const Grid: React.FC<GridProps> = ({
+  container = false,
+  item = true,
+  children,
+  ...props
+}) => {
+  const { classes } = useStyles();
+
+  if (container) {
+    return (
+      <Box {...props} className={classes.grid}>
+        {children}
+      </Box>
+    );
+  }
+
+  if (item) {
+    return <Box {...props}>{children}</Box>;
+  }
+
+  return null;
+};
+
+export default Grid;
+
+
+
+/*
 import Box, { BoxProps } from '@mui/material/Box';
 import { makeStyles } from 'tss-react/mui';
 
@@ -54,3 +100,4 @@ const Grid = ({
 };
 
 export default Grid;
+*/
